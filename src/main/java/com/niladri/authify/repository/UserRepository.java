a7ef6795240d17b52292c659938c75c7e2cf3eed
Niladri <niladrisengit12@gmail.com>
2025-11-23 01:27:21 +0530

    add custom authentication exception handling and define user profile interface

--- src/main/java/com/niladri/authify/repository/UserRepository.java
+++ src/main/java/com/niladri/authify/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.niladri.authify.repository;
 
 import com.niladri.authify.models.User;
-import com.niladri.authify.to.IProfile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import com.niladri.authify.to.IProfile;
 
 import java.util.Optional;
 
